da65163680a6525b90d644c0791f72531d185ed3
Halden Kavanagh <haldenkav@gmail.com>
2024-01-29 21:31:28 -0700

    addedfile structure for utils and comments to routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+// get to get all thoughts
+// get a single thought by id
+// post to create a new thought(push thought id to assosiated user thougts array field)
+// post to update thought by id
+// delete to remove a thought by id
+
+// /api/thoughts/:thoughtId/reactions
+// post to create a reaction stored in a single thought's array field
+// delete to remove a reaction
+
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+// get all users
+// get user by id
+// post a new user
+// put to update a user by id
+// delete a user
+
+// /api/users/:userId/friends/:friendId
+// post route to create a reaction stored in a single thought's reaction
+// delete route for a reaction based on id
+
 module.exports = router;
