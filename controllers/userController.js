b4755aa4f917ac6f5823344f60e484b9afa1a787
Halden Kavanagh <haldenkav@gmail.com>
2024-02-01 13:53:18 -0700

    started friend routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,4 +32,62 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  async putSingleUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with this id!" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async deleteSingleUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      await Application.deleteMany({ _id: { $in: user.applications } });
+      res.json({ message: "User and associated apps deleted!" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async addFriendToUser(req, res) {
+    const { userId, friendId } = req.params;
+
+    try {
+      const user = await User.findById(userId);
+      const friend = await User.findById(friendId);
+
+      if (!user || !friend) {
+        return res.status(404).json({ message: "User or friend not found" });
+      }
+
+      // Check if the friend is already in the user's friends list
+      if (!user.friends.includes(friendId)) {
+        // Add the friend to the user's friends list
+        user.friends.push(friendId);
+        await user.save();
+
+        return res.status(200).json({ message: "Friend added successfully" });
+      } else {
+        return res.status(400).json({ message: "Friend already added" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,16 +5,21 @@ const {
   createUser,
   putSingleUser,
   deleteSingleUser,
+  addFriendToUser,
 } = require("../../controllers/userController");
 
 // http://localhost:3001/api/users
 router.route("/").get(getUsers).post(createUser);
 
 // http://localhost:3001/api/users/:id
-router.route("/:userId").get(getSingleUser);
-//   .put(putSingleUser)
-//   .delete(deleteSingleUser);
+router
+  .route("/:userId")
+  .get(getSingleUser)
+  .put(putSingleUser)
+  .delete(deleteSingleUser);
 
+// http://localhost:3001/api/users/:userid/friends/friendId
+router.route("/:userId/friends/friendId").post(addFriendToUser);
 // /api/users/:userId/friends/:friendId
 // post route to create a reaction stored in a single thought's reaction
 // delete route for a reaction based on id
